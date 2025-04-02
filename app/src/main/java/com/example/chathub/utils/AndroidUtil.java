d27b46d08435953ca0ced796e5ca03863e356301
PrachiThanki <167408181+PrachiThanki@users.noreply.github.com>
2025-04-02 12:58:23 +0530

    'first'

--- app/src/main/java/com/example/chathub/utils/AndroidUtil.java
+++ app/src/main/java/com/example/chathub/utils/AndroidUtil.java
@@ -1,10 +1,19 @@
 package com.example.chathub.utils;
 
 import android.content.Context;
+import android.net.Uri;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class AndroidUtil {
     public static void showToast(Context context, String message){
         Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+    public static void setProffile(Context context, Uri imageUri, ImageView imageView){
+        Glide.with(context).load(imageUri).apply(RequestOptions.centerCropTransform()).into(imageView);
+
     }
 }
